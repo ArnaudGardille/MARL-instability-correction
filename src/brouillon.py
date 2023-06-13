da34ff68727a85e9992657df41158429735c752d
nono <arnaud.gardille@gmail.com>
2023-06-13 15:47:24 +0200

    before changing replay buffer

---
+++ src/brouillon.py
@@ -0,0 +1,22 @@
+from torchrl.data import TensorDictReplayBuffer
+from torchrl.data import LazyMemmapStorage, LazyTensorStorage, ListStorage
+
+# We define the maximum size of the buffer
+size = 10_000
+
+data = TensorDict(
+    {
+        "a": torch.arange(12).view(3, 4),
+        ("b", "c"): torch.arange(15).view(3, 5),
+    },
+    batch_size=[3],
+)
+print(data)
+
+buffer_lazymemmap = TensorDictReplayBuffer(
+    storage=LazyMemmapStorage(size, scratch_dir="/tmp/memmap/"), batch_size=12
+)
+buffer_lazymemmap.extend(data)
+print(f"The buffer has {len(buffer_lazymemmap)} elements")
+sample = buffer_lazymemmap.sample()
+print("sample:", sample)
--- src/water_bomber_env.py
+++ src/water_bomber_env.py
@@ -10,6 +10,7 @@ from pettingzoo.test import parallel_api_test
 
 factorielle = lambda n: n * factorielle(n-1) if n > 0 else 1
 import torch
+from random import randint
 import time
 
 class WaterBomberEnv(ParallelEnv):
@@ -17,27 +18,36 @@ class WaterBomberEnv(ParallelEnv):
     "name": "water-bomber-env_v0",
   }
 
-  def __init__(self):
+  def __init__(self, x_max=4, y_max=4, t_max=20, n_agents=2, deterministic=False):
 
-    self.X_MAX = 4
-    self.Y_MAX = 4
-    self.T_MAX = 20
-    self.N_AGENTS = 2
+    self.X_MAX = x_max
+    self.Y_MAX = y_max
+    self.T_MAX = t_max
+    self.N_AGENTS = n_agents
 
-    self.fires = []
-    self.water_bombers = []
+    self.deterministic = deterministic
+    if self.deterministic:
+      assert x_max==4 and y_max==4 and t_max==20 and n_agents==2
 
-    self.possible_agents = [0,1]
-    #["water_bomber_0", "water_bomber_1"]
+    self.possible_agents = ["water_bomber_"+str(i) for i in range(n_agents)]
+    self.symbols = {"water_bomber_"+str(i):str(i) for i in range(n_agents)}
     self.verbose = False
 
 
   def reset(self, seed=None, options=None):
     self.agents = copy(self.possible_agents)
 
-    self.fires = [[2,3],[4,3]]
-    #self.water_bombers = [[0,2],[2,2]]
-    self.water_bombers = [[0,2],[4,3]]
+    if self.deterministic:
+      self.fires = [[2,3],[4,3]]
+      self.water_bombers = {"water_bomber_0":[0,2],"water_bomber_1":[2,2]}
+    else:
+      points = {(randint(0, self.X_MAX), randint(0, self.Y_MAX))}
+      while len(points) < 2*self.N_AGENTS:
+        points |= {(randint(0, 4), randint(0, 4))}
+      list_pos = list(list(x) for x in points)
+      
+      self.fires = list_pos[:self.N_AGENTS]
+      self.water_bombers = {"water_bomber_"+str(i):coor for i, coor in enumerate(list_pos[self.N_AGENTS:])}
 
     self.has_finished = [False]*self.N_AGENTS
 
@@ -49,27 +59,27 @@ class WaterBomberEnv(ParallelEnv):
     return observations, infos
 
   def step(self, actions):
-    for i, action in actions.items():
+    for agent, action in actions.items():
 
-      x, y = self.water_bombers[i]
+      x, y = self.water_bombers[agent]
 
       if action == 0:
         assert y<self.Y_MAX
-        self.water_bombers[i][1] += 1
+        self.water_bombers[agent][1] += 1
       elif action == 1:
         assert x<self.X_MAX
-        self.water_bombers[i][0] += 1
+        self.water_bombers[agent][0] += 1
       elif action == 2:
         assert y>0
-        self.water_bombers[i][1] -= 1
+        self.water_bombers[agent][1] -= 1
       elif action == 3:
         assert x>0
-        self.water_bombers[i][0] -= 1
+        self.water_bombers[agent][0] -= 1
     
 
     #terminations = {a: self.water_bombers[a] in self.fires for a in self.agents}
 
-    self.has_finished = [self.water_bombers[a] in self.fires for a in self.agents]
+    self.has_finished = {a:self.water_bombers[a] in self.fires for a in self.agents}
 
     rewards = {a: self._compute_reward() for a in self.agents}
 
@@ -103,12 +113,12 @@ class WaterBomberEnv(ParallelEnv):
 
 
   def render(self):
-    grid = np.full((self.X_MAX+1, self.Y_MAX+1), '_')
+    grid = np.full((self.Y_MAX+1, self.X_MAX+1), '_')
     for x, y in self.fires:
       grid[y, x] = "F"
 
-    for i, (x, y) in enumerate(self.water_bombers):
-      grid[y, x] = str(i)
+    for agent, (x, y) in self.water_bombers.items():
+      grid[y, x] = self.symbols[agent]
 
     result = "\n".join(["".join([i for i in row]) for row in grid[::-1]])
     print()
@@ -117,7 +127,7 @@ class WaterBomberEnv(ParallelEnv):
   @functools.lru_cache(maxsize=None)
   def observation_space(self, agent):
       return Dict({
-        'observation': MultiDiscrete(sum([[self.X_MAX+1, self.Y_MAX+1] for _ in range(4)]+[[self.N_AGENTS, self.T_MAX]], [])), #+[13]
+        'observation': MultiDiscrete(sum([[self.X_MAX+1, self.Y_MAX+1] for _ in range(4)]+[[self.T_MAX]], [])), #+[13]
         'action_mask': MultiBinary(5)
       })
 
@@ -128,7 +138,7 @@ class WaterBomberEnv(ParallelEnv):
 
   def _is_terminated(self):
     fires = copy(self.fires)
-    water_bombers = copy(self.water_bombers)
+    water_bombers = copy(list(self.water_bombers.values()))
 
     fires.sort()
     water_bombers.sort()
@@ -137,29 +147,37 @@ class WaterBomberEnv(ParallelEnv):
 
   def _compute_reward(self):
     #if self._is_terminated():
-    if np.all(self.has_finished):
+    if np.all([self.has_finished[a] for a in self.agents]):
       return 1.0 #15.0
       #return 1.0-0.01*factorielle(self.timestep)
     else:
       return 0.0 #-1.0
 
   def get_action_mask(self, x, y):
+    #x, y = self.water_bombers[]
+
     action_mask = np.ones(5)
     if [x,y] in self.fires: #self.has_finished[agent]:
       action_mask = np.array([0,0,0,0,1])
     else:
       action_mask[-1] = 0
 
-    if y==self.Y_MAX or [x,y+1] in self.water_bombers:
+    occupied_positions = list(self.water_bombers.values())
+    if y==self.Y_MAX or [x,y+1] in occupied_positions:
       action_mask[0] = 0
-    if x==self.X_MAX or [x+1,y] in self.water_bombers:
+    if x==self.X_MAX or [x+1,y] in occupied_positions:
       action_mask[1] = 0
-    if y==0 or [x,y-1] in self.water_bombers:
+    if y==0 or [x,y-1] in occupied_positions:
       action_mask[2] = 0
-    if x==0 or [x-1,y] in self.water_bombers:
+    if x==0 or [x-1,y] in occupied_positions:
       action_mask[3] = 0
 
     return action_mask
+
+  def normalize_obs(self, obs):
+    agent = self.possible_agents[0]
+    normalized_obs = 2*obs.cpu()/(self.observation_space(agent)['observation'].nvec-1) - 1.0
+    return normalized_obs.float()
 
   def _generate_observations(self):
     action_masks = {}
@@ -168,9 +186,10 @@ class WaterBomberEnv(ParallelEnv):
 
       action_masks[agent] = self.get_action_mask(x,y)
 
+    occupied_positions = list(self.water_bombers.values())
     observations = {
       a: {
-        "observation":torch.flatten(torch.tensor(self.fires + self.water_bombers +[[a, self.timestep]], dtype=torch.float)), #+ [self.timestep]
+        "observation":torch.tensor(torch.tensor(sum(self.fires + occupied_positions +[[self.timestep]], []), dtype=torch.float)), #+ [self.timestep]
         "action_mask":action_masks[a]
       } 
       for a in self.agents
@@ -179,7 +198,7 @@ class WaterBomberEnv(ParallelEnv):
 
 
 if __name__ == "__main__":
-  env = WaterBomberEnv()
+  env = WaterBomberEnv(x_max=14, y_max=4, t_max=20, n_agents=2, deterministic=False)
   parallel_api_test(env, num_cycles=1_000_000)
 
   observations, _ = env.reset(seed=42)
