f989e9eea3c91476f46344c192f8835f6291d73b
nono <arnaud.gardille@gmail.com>
2023-06-26 14:56:47 +0200

    fusion

--- src/exec_stab.bash
+++ src/exec_stab.bash
@@ -5,6 +5,6 @@ rbs=('uniform')
 
 for rb in "${rbs[@]}"; do
   for ((run=0; run<=10; run+=1)); do
-    /home/nono/.conda/envs/torch_rl/bin/python "/home/nono/Documents/Dassault/Water-Bomber-Env/src/iql_new_rb_stab.py" --boltzmann-policy --corrected-loss --dueling --rb "$rb" --save-imgs --run-name "$rb-$run-stab-random"  --seed $run
+    /home/nono/.conda/envs/torch_rl/bin/python "/home/nono/Documents/Dassault/Water-Bomber-Env/src/iql_new_rb.py" --boltzmann-policy --corrected-loss --dueling --rb "$rb" --save-imgs --run-name "$rb-$run-stab-random"  --seed $run
   done
 done
