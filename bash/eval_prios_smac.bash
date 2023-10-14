94d1b3a4877ef6d0bc339022926ae0d4abb16552
nono <arnaud.gardille@gmail.com>
2023-10-14 17:34:11 +0200

    new exp

--- bash/eval_prios_smac.bash
+++ bash/eval_prios_smac.bash
@@ -5,6 +5,6 @@ configs=('--rb laber --prio cur-past --batch-size 1000' '--rb laber --prio cur -
 
 for ((run=0; run<1; run+=1)); do
   for ((i=0; i<3; i+=1)); do
-    /home/nono/.conda/envs/torch_rl/bin/python /home/nono/Documents/Dassault/Water-Bomber-Env/src/iql_gym.py --env-id lbf  --enforce-coop --run-name ${names[i]}-lbf-$run ${configs[i]} --fixed-buffer --load-buffer-from /home/nono/Documents/Dassault/Water-Bomber-Env/results/full_medium_lbf_2023-10-04_10:25:28 --buffer-size 5000000 --train-frequency 1 --total-timesteps 100000 --learning-starts 0 --learning-rate 0.0001 --device cpu --seed $run 
+    /home/nono/.conda/envs/torch_rl/bin/python /home/nono/Documents/Dassault/Water-Bomber-Env/src/iql_gym.py --env-id smac  --enforce-coop --run-name ${names[i]}-$run ${configs[i]} --fixed-buffer --load-buffer-from /home/nono/Documents/Dassault/Water-Bomber-Env/results/full_medium_smac_2023-10-03_12:23:02 --buffer-size 2000000 --train-frequency 1 --total-timesteps 100000 --learning-starts 0 --learning-rate 0.00001 --device cpu --seed $run 
   done
 done
