6c5b8769bf44328db481a8fe20094ff8cf7b3a87
ArnaudGardille <arnaud.gardille@gmail.com>
2023-10-14 17:51:34 +0200

    bash orga

---
+++ bash/mac/eval_prios_smac.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+names=('laber_cur_uncorrected' 'likely_cur' 'uniform' )
+configs=('--rb laber --prio cur-past --batch-size 1000' '--rb laber --prio cur --batch-size 1000' '--rb laber --prio cur-past --correct-prio --batch-size 1000' '--rb laber --prio cur --correct-prio --batch-size 1000' '--rb likely --prio cur-past --batch-size 1000' '--rb likely --prio cur --batch-size 1000' '--rb uniform --batch-size 10000')
+
+for ((run=0; run<1; run+=1)); do
+  for ((i=0; i<3; i+=1)); do
+    /Users/gardille/opt/anaconda3/envs/torch_rl/bin/python /Users/gardille/development/Water-Bomber-Env/src/iql_gym.py  --env-id smac  --enforce-coop --run-name ${names[i]}-$run ${configs[i]} --fixed-buffer --load-buffer-from /home/nono/Documents/Dassault/Water-Bomber-Env/results/full_medium_smac_2023-10-03_12:23:02 --buffer-size 2000000 --train-frequency 1 --total-timesteps 100000 --learning-starts 0 --learning-rate 0.00001 --device cpu --seed $run 
+  done
+done
